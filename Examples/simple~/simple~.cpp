72eb9bc9a6406517020f4c5af5062305f4ca4d6f
Charles K. Neimog <charlesneimog@outlook.com>
2025-09-26 13:27:11 -0300

    minor update to object

--- Examples/simple~/simple~.cpp
+++ Examples/simple~/simple~.cpp
@@ -2,41 +2,41 @@
 
 static t_class *simple_tilde_class;
 
-class SimpleTilde {
+class simple_tilde {
   public:
-    t_object xObj;
-    t_sample xSample;
-    t_float nMultiplier;
-    t_outlet *xOutlet;
+    t_object xobj;
+    t_sample s;
+    t_float n_multiplier;
+    t_outlet *x_out;
 };
 
 // ─────────────────────────────────────
-static void SimpleFloat(SimpleTilde *x, t_float f1) { x->nMultiplier = f1; }
+static void simple_tilde_float(simple_tilde *x, t_float f1) { x->n_multiplier = f1; }
 
 // ─────────────────────────────────────
-static t_int *SimplePerform(t_int *w) {
-    SimpleTilde *x = (SimpleTilde *)(w[1]);
+static t_int *simple_tilde_perform(t_int *w) {
+    simple_tilde *x = (simple_tilde *)(w[1]);
     t_sample *in = (t_sample *)(w[2]);
     t_sample *out = (t_sample *)(w[3]);
     int n = (int)(w[4]);
 
     for (int i = 0; i < n; i++) {
-        out[i] = in[i] * x->nMultiplier;
+        out[i] = in[i] * x->n_multiplier;
     }
 
     return (w + 5);
 }
 
 // ─────────────────────────────────────
-static void AddDsp(SimpleTilde *x, t_signal **sp) {
-    dsp_add(SimplePerform, 4, x, sp[0]->s_vec, sp[1]->s_vec, sp[0]->s_n);
+static void simple_tilde_dsp(simple_tilde *x, t_signal **sp) {
+    dsp_add(simple_tilde_perform, 4, x, sp[0]->s_vec, sp[1]->s_vec, sp[0]->s_n);
 }
 
 // ─────────────────────────────────────
 static void *simple_tilde_new(void) {
-    SimpleTilde *x = (SimpleTilde *)pd_new(simple_tilde_class);
-    x->xOutlet = outlet_new(&x->xObj, &s_signal);
-    x->nMultiplier = 0.3;
+    simple_tilde *x = (simple_tilde *)pd_new(simple_tilde_class);
+    x->x_out = outlet_new(&x->xobj, &s_signal);
+    x->n_multiplier = 0.3;
     return x;
 }
 
@@ -44,8 +44,8 @@ static void *simple_tilde_new(void) {
 extern "C" void simple_tilde_setup(void) {
     simple_tilde_class = class_new(gensym("simple~"), simple_tilde_new, 0,
                                    sizeof(t_object), 0, A_NULL);
-    CLASS_MAINSIGNALIN(simple_tilde_class, SimpleTilde, xSample);
-    class_addmethod(simple_tilde_class, (t_method)AddDsp, gensym("dsp"), A_CANT,
+    CLASS_MAINSIGNALIN(simple_tilde_class, simple_tilde, s);
+    class_addmethod(simple_tilde_class, (t_method)simple_tilde_dsp, gensym("dsp"), A_CANT,
                     0);
-    class_addfloat(simple_tilde_class, (t_method)SimpleFloat);
+    class_addfloat(simple_tilde_class, (t_method)simple_tilde_float);
 }
